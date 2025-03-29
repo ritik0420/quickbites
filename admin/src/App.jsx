001b9f28209e4d3c089a204f266b3b3d2cf48994
Ritik <ritik.kaintura007@gmail.com>
2025-03-29 13:18:56 +0530

    Updated url

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -10,7 +10,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url='https://quickbites-qltf.vercel.app/'
+  const url='https://quickbites-qltf.vercel.app'
 
   return (
     <div>
